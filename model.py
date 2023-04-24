184c1bed7f490fd93381a7e6f15386a6045cca6f
james tipton <jctppc@umsystem.edu>
2023-04-24 16:09:41 -0500

    python file

--- model.py
+++ model.py
@@ -4,11 +4,11 @@ from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score, confusion_matrix
 
 # Load the dataset
-data = pd.read_csv('dataset.csv')
+data = pd.read_csv('depression.csv')
 
 # Split the data into features (X) and labels (y)
-X = data.drop('label', axis=1)
-y = data['label']
+X = data.drop('clean_text', axis=1)
+y = data['is_depression']
 
 # Split the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
